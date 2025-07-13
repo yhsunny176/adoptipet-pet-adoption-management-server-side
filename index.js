71e688fa7618dea6d527b688c596fd40d0f3713d
yhsunny176 <yeasinulhaq763@gmail.com>
2025-07-13 12:55:24 +0600

    JWT authentication setup has been done and secured the access token

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
@@ -22,19 +29,47 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
-    // Connect client to the server
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        const db = client.db("AdoptiPetDB");
+        const petCollection = db.collection("allpets");
+
+        //generate jwt
+        app.post("/jwt", (req, res) => {
+            const email = req.body.email;
+
+            if (!email) {
+                return res.status(400).send({ success: false, message: "Email is required" });
+            }
+
+            //token creation
+            const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "20d" });
+            res.cookie("token", accessToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            }).send({ success: true });
+        });
+        // Logout
+        app.get("/logout", async (req, res) => {
+            try {
+                res.clearCookie("token", {
+                    maxAge: 0,
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                }).send({ success: true });
+            } catch (err) {
+                res.status(500).send(err);
+            }
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
 }
 run();
-
 
 // Basic route
 app.get("/", (req, res) => {
